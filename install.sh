d38dccc9ac973ee73ff67f7b2df9ec6e20a8e437
Marcel Baumgartner <68913099+Marcel-Baumgartner@users.noreply.github.com>
2023-04-17 21:16:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Installing cloud panel api server"
 
 mkdir /lib/cpapiserver/
 
-curl -L -o /lib/cpapiserver/CloudPanelApi "https://github.com/Endelon-Hosting/CloudPanelApi/releases/latest/download/CloudPanelApi_linux_([[ "$(uname -m)" == "x86_64" ]] && echo "arm64")"
-curl -L -o /lib/cpapiserver/CloudPanelApi.pdb "https://github.com/Endelon-Hosting/CloudPanelApi/releases/latest/download/cpapiserver_linux_([[ "$(uname -m)" == "x86_64" ]] && echo "arm64")"
+curl -L -o /lib/cpapiserver/CloudPanelApi "https://github.com/Endelon-Hosting/CloudPanelApi/releases/latest/download/CloudPanelApi_linux_$(uname -m)"
+curl -L -o /lib/cpapiserver/CloudPanelApi.pdb "https://github.com/Endelon-Hosting/CloudPanelApi/releases/latest/download/cpapiserver_linux_$(uname -m)"
 curl -L -o /lib/cpapiserver/appsettings.json https://raw.githubusercontent.com/Endelon-Hosting/CloudPanelApi/main/appsettings.json
 
 curl -L -o /etc/systemd/system/cpapiserver.service https://raw.githubusercontent.com/Endelon-Hosting/CloudPanelApi/main/cpapiserver.service
